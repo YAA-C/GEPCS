f6cced9da931d29fc9bb3aaffa3d468bcb3e258f
SiddheshDhinge <saddy97531@gmail.com>
2023-12-11 23:34:16 +0530

    Modified ./demoparserLib to accomodate bot actions

---
+++ demoparserLib/src/parsing.rs
@@ -0,0 +1,10 @@
+pub mod collect_data;
+pub mod data_table;
+pub mod entities;
+pub mod game_events;
+pub mod header;
+pub mod parser;
+pub mod read_bits;
+pub mod read_bytes;
+pub mod stringtables;
+pub mod variants;
